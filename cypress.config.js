3ca8838a3a92aa40450331984a02adf3367b4dc0
Tiait1999 <aitzianethinhinane@gmail.com>
2026-07-16 11:37:25 +0200

    jenkinsfile

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,27 @@
 const { defineConfig } = require("cypress");
-const cucumber = require("cypress-cucumber-preprocessor").default; 
+const cucumber = require("cypress-cucumber-preprocessor").default;
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
 
 module.exports = defineConfig({
   allowCypressEnv: true,
+
+  env: {
+  allure: true,
+  allureResultsPath: 'allure-results',
+  allureReuseAfterSpec: true,
+  },
 
   e2e: {
     specPattern: "**/*.feature", // a ajouter 
     setupNodeEvents(on, config) {
       // implement node event listeners here
     on("file:preprocessor", cucumber()); // ajouter pour le preprocessor
+    allureWriter(on, config);
+    return config
+    
     
     },
+      
   },
 });
--- cypress/steps/inscription.step.js
+++ cypress/steps/inscription.step.js
@@ -1,12 +1,13 @@
 import {Given, When, Then} from "cypress-cucumber-preprocessor/steps";
 
-
 Given('je suis sur le site {string}', (url) => {
  cy.visit(url)
 })
+
 When('je clique sur le boutton creer compte', () => {
   cy.get('#nav-register').click()
 })
+
 Then('je me rederige vers la page {string}', (url) => {
   cy.visit(url)
 })
@@ -20,14 +21,17 @@ When('je saisie le nom', () => {
   let n=Math.random().toString(36).substring(2, 15);
   cy.get('#register-nom').type(n)
 })
+
 When('je saisie l email', () => {
   let e=Math.random().toString(36).substring(2, 15) + "@gmail.com";
   cy.get('#register-email').type(e)
 })
+
 When('je saisie le date de naissance', () => {
   let b="1999-02-08";
   cy.get('#register-date-naissance').type(b)
 })
+
 When('je saisie le password', () => {
   let pass="Tina1234@"
   cy.get('#register-password').type(pass)
@@ -37,6 +41,7 @@ When('je confirme le password', () => {
   let cpass="Tina1234@"
   cy.get('#register-password-confirm').type(cpass)
 })
+
 When('je choisi le type de compte', () => {
   cy.get('#register-role').select('Client')
 })
